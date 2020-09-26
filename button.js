83220584611091a765ca40b6168f482ec9634795
Victoria Emerson <valeevtim@gmail.com>
2020-09-26 16:55:40 +0300

    add panelbuilder

--- button.js
+++ button.js
@@ -7,12 +7,14 @@ let description = document.getElementById("project-description");
 var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
 let currentProject = 0;
-let totalProjects = 2;
+let totalProjects = 3;
 
-let images = ['research.jpeg', 'assembly-simulation.png'];
-let titles = ['Research on Numerical Methods', 'Assembly Simulation'];
-let descriptions = ["Comparative analysis of Euler, Euler-Richardson, Verlet and Runge-Kutta methods. A testing model was developed and the system was simulated, then produced relative error functions were approximated with linear regression.",
-"A simulation of an assembly system that I proposed while working at ICL Techo. Instead of assembling computers consecutively, every member of the brigade assembles individually. With 6 people in the brigade, 232 seconds for full assembly, and 5 seconds for handover, the proposed system's efficiency is better by 28%."];
+let images = ['research.jpeg', 'assembly-simulation.png', 'panelbuilder.png'];
+let titles = ['<a href="https://github.com/escape13/research" target="_blank">Research on Numerical Methods</a>', '<a href="https://github.com/escape13/assembly-simulation" target="_blank">Assembly Simulation</a>', '<a href="https://github.com/escape13/PanelBuilder" target="_blank">PanelBuilder</a>'];
+let descriptions = ['Comparative analysis of Euler, Euler-Richardson, Verlet and Runge-Kutta methods. A testing model was developed and the system was simulated, then produced relative error functions were approximated with linear regression. <a href="https://github.com/escape13/research/blob/master/analysis_eng.pdf" target="_blank">Click here to read.</a>',
+"A simulation of an assembly system that I proposed while working at ICL Techno. Instead of assembling computers consecutively, every member of the brigade assembles individually. With 6 people in the brigade, 232 seconds for full assembly, and 5 seconds for handover, the proposed system's efficiency is better by 28%.",
+"An add-on for Blender 3D. Modeling paneled surfaces made easy. Developed while working in SouyzKhimPromProekt as a 3D artist. The petrochemical facility I was modeling had many similar paneled structures, so I wrote this add-on to automate the proccess of paneled surfaces generation."
+];
 
 function update(index) {
     image.src = `src/images/${images[index]}`;
