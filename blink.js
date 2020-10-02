e011d21e5a8e47cbe3b65ecdcc395122900bb9a6
Victoria Emerson <valeevtim@gmail.com>
2020-10-02 23:23:27 +0300

    gender transition

--- blink.js
+++ blink.js
@@ -1,6 +1,6 @@
 $(function () { 
   count = 0; 
-  wordsArray = ["Timur Valeev", "escape13"]; 
+  wordsArray = ["Victoria Valeeva", "escape13"]; 
   setInterval(function () { 
     count++; 
     $("#name").fadeOut(150, function () { 
