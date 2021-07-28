22dfa4a3d87d17b38ba9365605f17d749821d7d7
Timur Valeev <valeevtim@gmail.com>
2021-07-28 04:28:05 +0300

    typewriter fix

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -10,7 +10,7 @@ var textArray = [
 ];
 
 // Speed (in milliseconds) of typing.
-var speedForward = 300, //Typing Speed
+var speedForward = 500, //Typing Speed
     speedWait = 1000, // Wait between typing and backspacing
     speedBetweenLines = 1000, //Wait between first and second lines
     speedBackspace = 25; //Backspace Speed
