d4daf9dafab098cbaf3f6a578dc614a97c7e008b
Victoria Valeeva <vicky.valeeva@gmail.com>
2024-05-03 17:48:40 -0400

    fix mobile

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth / 2, window.innerHeight / 2);
 document.body.appendChild( renderer.domElement );
 
 const light1 = new THREE.PointLight( 0xffffff, 10, 100 );
