5abb3643691220e4e65d523737baec434c30a01b
Victoria Valeeva <vicky.valeeva@gmail.com>
2024-05-03 16:40:22 -0400

    fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from 'node_modules/three/build/three.module.js';
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
