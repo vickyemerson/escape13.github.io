7660df9810a19d93cc667dfb1603fe11fde2f3e5
Victoria Valeeva <vicky.valeeva@gmail.com>
2024-05-03 18:52:23 -0400

    fixed

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from './three/build/three.module.js';
-import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from '/three/build/three.module.js';
+import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -20,6 +20,7 @@ camera.position.z = 1.75;
 
 camera.lookAt(new THREE.Vector3(0, 0 , 0));
 
+
 const loader = new GLTFLoader();
 
 loader.load( '/protein.glb', function ( gltf ) {
@@ -39,4 +40,4 @@ loader.load( '/protein.glb', function ( gltf ) {
 
 	console.error( error );
 
-} );
+} ); 
--- three/examples/jsm/utils/BufferGeometryUtils.js
+++ three/examples/jsm/utils/BufferGeometryUtils.js
@@ -9,7 +9,7 @@ import {
 	TriangleStripDrawMode,
 	TrianglesDrawMode,
 	Vector3,
-} from 'three';
+} from '/three/build/three.module.js';
 
 function computeMikkTSpaceTangents( geometry, MikkTSpace, negateSign = true ) {
 
