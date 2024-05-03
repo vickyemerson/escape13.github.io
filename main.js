c91f709ee31223eab41634fb0d8078867c55b0d8
Victoria Valeeva <vicky.valeeva@gmail.com>
2024-05-03 18:32:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import * as THREE from './three/build/three.module.js';
-// import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth / 2, window.innerHeight / 2);
+renderer.setSize( window.innerWidth, window.innerHeight);
 document.body.appendChild( renderer.domElement );
 
 const light1 = new THREE.PointLight( 0xffffff, 10, 100 );
@@ -19,8 +19,6 @@ scene.add( light2 );
 camera.position.z = 1.75;
 
 camera.lookAt(new THREE.Vector3(0, 0 , 0));
-
-/*
 
 const loader = new GLTFLoader();
 
@@ -42,21 +40,3 @@ loader.load( '/protein.glb', function ( gltf ) {
 	console.error( error );
 
 } );
-
-*/
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-}
-
-animate();
